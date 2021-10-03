9c07ebc072e2baff175c45de506db8f92aba58d6
Plume羽 <38199330+Plumie@users.noreply.github.com>
2021-10-03 21:39:28 +0200

    feat: fix text and added combo sfx

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -8,6 +8,7 @@ public class ScoreManager : MonoBehaviour
     public float defaultScore;
     public TMP_Text scoreUI;
     public TMP_Text comboUI;
+    public AudioSource comboSound;
     float score;
     float combo = 1f;
     GameObject lastProp;
@@ -16,8 +17,11 @@ public class ScoreManager : MonoBehaviour
     {
         if(comboing)
         {
+            comboSound.Play();
+            comboSound.pitch = comboSound.pitch + 0.1f; 
             combo = combo + 1f;
         } else {
+            comboSound.pitch = 1f;
             combo = 1f;
         }
         score = score + combo;
